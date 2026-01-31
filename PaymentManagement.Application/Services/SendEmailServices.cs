3f2dfaf3c0f665b478c62ce29389f4722465ff89
Oussama_Wahbi <wahbi.oussama08@gmail.com>
2026-01-31 13:49:12 +0100

    -m

--- PaymentManagement.Application/Services/SendEmailServices.cs
+++ PaymentManagement.Application/Services/SendEmailServices.cs
@@ -1,16 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
+using System.Text.Json;
+using PaymentManagement.Application.DTO;
+using PaymentManagement.Domain;
 
 namespace PaymentManagement.Application.Services
 {
     public class SendEmailServices
     {
-        public async Task SendEmailAsync()
+        public async Task SendEmailAsync(InvoiceEmailRequest emailInfo)
         {
+            var client = new HttpClient();
 
+            string jsonData = JsonSerializer.Serialize(emailInfo);
+            var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var respose = await client.PostAsync("http//localhost:1212/email/invoice", content);
         }
     }
 }
